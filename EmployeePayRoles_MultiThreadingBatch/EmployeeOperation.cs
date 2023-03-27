8d8263168fffb15c12c49e612119fbe507b4a3be
Snehal979 <bansodsnehal535@gmail.com>
2023-03-27 23:57:01 +0530

    [Snehal]Add: Add employee single data added

--- EmployeePayRoles_MultiThreadingBatch/EmployeeOperation.cs
+++ EmployeePayRoles_MultiThreadingBatch/EmployeeOperation.cs
@@ -42,5 +42,60 @@ namespace EmployeePayRoles_MultiThreadingBatch
                 thread.Start();
             }
         }
+        /// <summary>
+        /// UC3 added data for single Employee Without Threading
+        /// </summary>
+        /// <param name="employeeList"></param>
+        public void SingleAddEmployee(List<Employee> employeeList)
+        {
+            Console.WriteLine("Enter the Id");
+            int Id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the firstName");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Enter the Salary");
+            long Salary = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter the Gender");
+            String Gendr = Console.ReadLine();
+
+            Employee employeecs = new Employee(Id, Name, Salary, Gendr);
+            Console.WriteLine(" Employee being added "+Name);
+            Console.WriteLine("id being added"+Id);
+            Console.WriteLine("Salary being added"+Salary);
+            Console.WriteLine("Gender being added"+Gendr);
+            employeeList.Add(employeecs);
+            Console.WriteLine("Employee added "+Name);
+            Console.WriteLine("id added"+Id);
+            Console.WriteLine("Salary added"+Salary);
+            Console.WriteLine("Gender added"+Gendr);
+        }
+        /// <summary>
+        /// UC3 added data for single Employee Without Threading
+        /// </summary>
+        /// <param name="employeeList"></param>
+        public void SingleAddEmployee_WithThread(List<Employee> employeeList)
+        {
+            Console.WriteLine("Enter the Id");
+            int Id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the firstName");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Enter the Salary");
+            long Salary = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter the Gender");
+            String Gendr = Console.ReadLine();
+            // Task is Use for parallel
+            Task task = new Task(() =>
+            {
+                Employee employeecs = new Employee(Id, Name, Salary, Gendr);
+                Console.WriteLine(" Employee being added "+Name);
+                Console.WriteLine("id being added"+Id);
+                Console.WriteLine("Salary being added"+Salary);
+                Console.WriteLine("Gender being added"+Gendr);
+                employeeList.Add(employeecs);
+                Console.WriteLine("Employee added "+Name);
+                Console.WriteLine("id added"+Id);
+                Console.WriteLine("Salary added"+Salary);
+                Console.WriteLine("Gender added"+Gendr);
+            });
+        }
     }
 }
--- EmployeePayRoles_MultiThreadingBatch/Program.cs
+++ EmployeePayRoles_MultiThreadingBatch/Program.cs
@@ -12,17 +12,42 @@
             employee.Add(new Employee(1, "Vaibhav", 30535352, "Male"));
             employee.Add(new Employee(1, "Raju", 5000052, "Male"));
             employee.Add(new Employee(1, "Snehu", 199999, "Female"));
+            Console.WriteLine("Employee pay role program running using multi threading ");
+            Console.WriteLine("Hint 1.Display data without thread 2.DisplayData with thread 3. Add data without threa 4.Add data with Thread");
+            int number = Convert.ToInt32(Console.ReadLine());
 
-            ////Uc1 Without Thread
-            DateTime starttime = DateTime.Now;
-            employeeOperation.addEmployeeToPayRole(employee);
-            DateTime endtime = DateTime.Now;
-            Console.WriteLine("Duration without threading is "+(endtime-starttime));
-            ////Uc 2 with thread
-            DateTime starttimeThread = DateTime.Now;
-            employeeOperation.addEmployeeToPayRole_WithThread(employee);
-            DateTime endtimeThread = DateTime.Now;
-            Console.WriteLine("Duration with threading is "+(endtimeThread-starttimeThread));
+            switch (number)
+            {
+                case 1:
+                    ////Uc1 Without Thread
+                    DateTime starttime = DateTime.Now;
+                    employeeOperation.addEmployeeToPayRole(employee);
+                    DateTime endtime = DateTime.Now;
+                    Console.WriteLine("Duration without threading is "+(endtime-starttime));
+                    break;
+                case 2:
+                    ////Uc 2 with thread
+                    DateTime starttimeThread = DateTime.Now;
+                    employeeOperation.addEmployeeToPayRole_WithThread(employee);
+                    DateTime endtimeThread = DateTime.Now;
+                    Console.WriteLine("Duration with threading is "+(endtimeThread-starttimeThread));
+                    break;
+                case 3:
+                    ////Uc3 Without Thread
+                    DateTime starttimeADD = DateTime.Now;
+                    employeeOperation.SingleAddEmployee(employee);
+                    DateTime endtimeADD = DateTime.Now;
+                    Console.WriteLine("Duration without threading is "+(endtimeADD-starttimeADD));
+                    break;
+                case 4:
+                    Console.WriteLine("Enter data for Threading");
+                    //// Uc 3 with thread
+                    DateTime starttimeThreadADD = DateTime.Now;
+                    employeeOperation.SingleAddEmployee_WithThread(employee);
+                    DateTime endtimeThreadADD = DateTime.Now;
+                    Console.WriteLine("Duration with threading is "+(endtimeThreadADD-starttimeThreadADD));
+                    break;
+            }
         }
     }
 }
